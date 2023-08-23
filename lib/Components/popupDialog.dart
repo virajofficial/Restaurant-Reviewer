d482af61274096d644ef3cdb69f9304ee7cec183
Sahan Perera <sahan@spadesapp.io>
2023-08-24 00:41:52 +0530

    review popup dialog done

--- lib/Components/popupDialog.dart
+++ lib/Components/popupDialog.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class PopupDialog extends StatelessWidget {
@@ -16,6 +18,7 @@ class PopupDialog extends StatelessWidget {
           minHeight: 500,
         ),
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
             Container(
               width: double.maxFinite,
@@ -30,10 +33,10 @@ class PopupDialog extends StatelessWidget {
                     color: Color(0xFFBD712B),
                     width: 3,
                   )),
-                  // borderRadius: BorderRadius.only(
-                  //   topLeft: Radius.circular(10),
-                  //   topRight: Radius.circular(10),
-                  // ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
                   boxShadow: [
                     BoxShadow(
                       color: Color.fromARGB(150, 0, 0, 0),
@@ -42,49 +45,138 @@ class PopupDialog extends StatelessWidget {
                       blurRadius: 3,
                     ),
                   ]),
-              child: const Column(
-                children: [
-                  Text('Review Title'),
-                  Text('Restaurant Name'),
-                  Text('Customer Name')
-                ],
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Review Title',
+                        style: TextStyle(
+                          color: Color(0xFFCD3100),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Text(
+                      'Restaurant Name',
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '( By ',
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Color(0xFF783B0F),
+                              fontStyle: FontStyle.italic),
+                        ),
+                        Text(
+                          'Customer Name ',
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Color(0xFF783B0F),
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          ')',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color(0xFF783B0F),
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
             Container(
-              width: double.maxFinite,
-              height: 412,
-              decoration: const BoxDecoration(
-                  gradient: RadialGradient(
-                    colors: [
-                      Color(0xFFF1C9A5),
-                      Color.fromARGB(255, 239, 177, 123)
-                    ],
-                  ),
-                  border: Border(
-                      left: BorderSide(
-                    color: Color(0xFFBD712B),
-                    width: 3,
-                  )),
-                  // borderRadius: BorderRadius.only(
-                  //   topLeft: Radius.circular(10),
-                  //   topRight: Radius.circular(10),
-                  // ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromARGB(150, 0, 0, 0),
-                      blurStyle: BlurStyle.outer,
-                      offset: Offset(0, 00.1),
-                      blurRadius: 3,
+                width: double.maxFinite,
+                height: 412,
+                decoration: const BoxDecoration(
+                    gradient: RadialGradient(
+                      colors: [
+                        Color(0xFFF1C9A5),
+                        Color.fromARGB(255, 239, 177, 123)
+                      ],
                     ),
-                  ]),
-              child: const Column(
-                children: [
-                  Text('Review Title', textAlign: TextAlign.start),
-                  Text('Restaurant Name'),
-                  Text('Customer Name')
-                ],
-              ),
-            )
+                    border: Border(
+                      left: BorderSide(
+                        color: Color(0xFFBD712B),
+                        width: 3,
+                      ),
+                      right: BorderSide(
+                        color: Color(0xFFBD712B),
+                        width: 3,
+                      ),
+                      bottom: BorderSide(
+                        color: Color(0xFFBD712B),
+                        width: 3,
+                      ),
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(150, 0, 0, 0),
+                        blurStyle: BlurStyle.outer,
+                        offset: Offset(0, 00.1),
+                        blurRadius: 3,
+                      ),
+                    ]),
+                child: Column(
+                  children: [
+                    const Expanded(
+                      child: SingleChildScrollView(
+                        padding: EdgeInsets.only(
+                          left: 60,
+                          right: 60,
+                          top: 20,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic v v Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic v Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic Description sd fa tr sers  sdf e rs  sf s te rts  s ts fd s f sdf s f sdf s  s df s f s f sdf , fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic, fontStyle: FontStyle.italic ',
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 380,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xFFA83131)),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Cancel',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    )
+                  ],
+                ))
           ],
         ),
       ),
